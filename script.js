26a12924855d3b9e9b1982532c7a3455359e02b1
Sulic Octavian <octavius1992@gmail.com>
2022-10-06 15:45:40 +0300

    Add js

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
+const playerConstruct = (playerTag) => {
+    const playerInput = document.getElementById(`${playerTag}`);
+    const name = () =>`Player ${playerTag}`;
+    let symbol = playerTag;
+    return {symbol, name};
+};
+
+let PlayerX = playerConstruct('X');
+let Player0 = playerConstruct('O');
+
 const gameBoard = (() => {
-    const Board = [];
-    return {Board};
-})()
+    const squares = Array.from(document.querySelectorAll('.square'));
+    const choices = ['', '', '', '', '', '', '', '', ''];
+
+    squares.forEach(square => {
+        square.addEventListener('click', function() {
+            if (this.innerHTML == '') {
+                if (game.turnOf === 'X') {
+                    this.innerHTML = PlayerX.symbol;
+                    game.turnOf = 'O';
+                    choices[squares.indexOf(this)] = this.innerHTML;
+                    game.test();
+                } else {
+                    this.innerHTML = Player0.symbol;
+                    game.turnOf = 'X';
+                    choices[squares.indexOf(this)] = this.innerHTML;
+                }
+            } else {alert('Taken!')};
+        })
+    })
+
+    return {squares, choices};
+})();
+
+const game = (() => {
+    let turnOf = 'X';
+
+    // const gameStatus = () => {
+    // }
+
+    
+    return {turnOf};
+})();
