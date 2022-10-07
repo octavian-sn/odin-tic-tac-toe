bec772df0b2a2a17855aeaee9cb05280f494aba1
Sulic Octavian <octavius1992@gmail.com>
2022-10-07 16:45:08 +0300

    Add modal and fix name-change

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const player = (playerTag) => {
-    const playerInput = document.getElementById(`${playerTag}`);
-    let name = () =>`Player ${playerTag}`;
+    const input = document.getElementById(`${playerTag}`);
+    let name = `Player ${playerTag}`;
     let symbol = playerTag;
     let score = 0;
-    return {symbol, name, score};
+    return {symbol, name, score, input};
 };
 
 const board = (() => {
@@ -22,16 +22,16 @@ const board = (() => {
             if (this.innerHTML == '') {
                 if (game.turnOf === 'X') {
                     this.innerHTML = PlayerX.symbol;
-                    game.turnOf = 'O';
                     choices[squares.indexOf(this)] = this.innerHTML;
                     ++PlayerX.score;
-                    PlayerX.score >= 3 ? game.checkWinner() : '';
+                    PlayerX.score >= 3 ? game.checkWinner(PlayerX.name) : '';
+                    game.turnOf = 'O';
                 } else {
                     this.innerHTML = PlayerO.symbol;
                     game.turnOf = 'X';
-                    choices[squares.indexOf(this)] = this.innerHTML;
                     ++PlayerO.score;
-                    PlayerO.score >= 3 ? game.checkWinner() : '';
+                    choices[squares.indexOf(this)] = this.innerHTML;
+                    PlayerO.score >= 3 ? game.checkWinner(PlayerO.name) : '';
                 }
             } else {alert('Taken!')};
         })
@@ -40,59 +40,111 @@ const board = (() => {
     return {squares, choices, newBoard};
 })();
 
+let PlayerX = player('X');
+let PlayerO = player('O');
+
 const game = (() => {
+    // Player turn counter
     let turnOf = 'X';
-    let message = `The winner is: Player${turnOf}`;
-    const button = document.getElementById('new-game');
-    button.addEventListener('click', () => {
+    let winner = '';
+
+    // New game
+    const newGame = () => {
         board.newBoard()
         PlayerX.score = 0;
         PlayerO.score = 0;
         game.turnOf = 'X';
-    });
-    
+    };
 
-    const checkWinner = () => {
+    // Change names of players
+    PlayerX.input.addEventListener('input', function(){
+        PlayerX.name = this.value;
+        if (PlayerX.name == '') {PlayerX.name = 'PlayerX'};
+    });
+    PlayerO.input.addEventListener('input', function(){
+        PlayerO.name = this.value;
+        if (PlayerO.name == '') {PlayerO.name = 'PlayerO'};
+    });
+
+    const checkWinner = (name) => {
         // Horizontal rows
         if (board.choices[0] === board.choices[1] && board.choices[1] === board.choices[2] &&
             board.choices[2] !== '') {
-            alert(message);
+            game.winner = `The winner is: ${name}`;
+            modal.open();
         } else if (board.choices[3] === board.choices[4] && board.choices[4] === board.choices[5] &&
             board.choices[5] !== ''){
-            alert(message);
+            game.winner = `The winner is: ${name}`;
+            modal.open();
         } else if (board.choices[6] === board.choices[7] && board.choices[7] === board.choices[8] &&
             board.choices[8] !== ''){
-            alert(message);
+            game.winner = `The winner is: ${name}`;
+            modal.open();
         }
         // Vertical rows
           else if (board.choices[0] === board.choices[3] && board.choices[3] === board.choices[6] &&
             board.choices[6] !== ''){
-            alert(message);
+            game.winner = `The winner is: ${name}`;
+            modal.open();
         } else if (board.choices[1] === board.choices[4] && board.choices[4] === board.choices[7] &&
             board.choices[7] !== ''){
-            alert(message);
+            game.winner = `The winner is: ${name}`;
+            modal.open();
         } else if (board.choices[2] === board.choices[5] && board.choices[5] === board.choices[8] &&
             board.choices[8] !== ''){
-            alert(message);
+            game.winner = `The winner is: ${name}`;
+            modal.open();
         }
         // Diagonal rows
           else if (board.choices[0] === board.choices[4] && board.choices[4] === board.choices[8] &&
             board.choices[8] !== ''){
-            alert(message);
+            game.winner = `The winner is: ${name}`;
+            modal.open();
         } else if (board.choices[2] === board.choices[4] && board.choices[4] === board.choices[6] &&
             board.choices[6] !== ''){
-            alert(message);
+            game.winner = `The winner is: ${name}`;
+            modal.open();
         } 
         // Tie
           else if (PlayerX.score === 5 && PlayerO.score === 4) {
-            alert('Tie');
+            game.winner = `It's a tie!`;
+            modal.open();
         } else if (PlayerX.score === 4 && PlayerO.score === 5) {
-            alert('Tie');
+            game.winner = `It's a tie!`;
+            modal.open();
         }
     }
 
-    return {turnOf, checkWinner};
+    return {turnOf, checkWinner, newGame, winner};
 })();
 
-let PlayerX = player('X');
-let PlayerO = player('O');
+const modal = (() => {
+    const modal = document.getElementById('modal');
+    const modalMessage = document.getElementById('modal__window');
+    const overlay = document.getElementById('overlay');
+    const restart = document.getElementById('restart'); 
+    const button = document.getElementById('modal__button');
+    button.addEventListener('click', () => {
+        game.newGame();
+        close();
+    });
+    overlay.addEventListener('click', close);
+    restart.addEventListener('click', () => {game.newGame()});
+
+    // Open pop-up
+    function open() {
+        console.log(game.winner);
+        modalMessage.innerHTML = game.winner;
+        if (modal == null) return
+        modal.classList.add('active')
+        overlay.classList.add('active')
+    }
+    // Close pop-up
+    function close() {
+        if (modal == null) return
+        modal.classList.remove('active')
+        overlay.classList.remove('active')
+    }
+
+    return {button, open};
+})()
